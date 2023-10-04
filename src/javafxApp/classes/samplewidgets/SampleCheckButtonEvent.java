c073ea89f8e4230680b266c09198fc2de1c6b68c
Keiichi Takahashi <keiichi.takahashi@tel.com>
2023-10-04 10:43:18 +0900

    update

---
+++ src/javafxApp/classes/samplewidgets/SampleCheckButtonEvent.java
@@ -0,0 +1,26 @@
+package samplewidgets;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.CheckBox;
+
+public class SampleCheckButtonEvent implements EventHandler<ActionEvent> {
+
+    @Override
+    public void handle(ActionEvent event) {
+        CheckBox cb = (CheckBox) event.getSource();
+        String cbText = cb.getText();
+
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Event");
+
+        if (cb.isSelected()) {
+            alert.setContentText(cbText + ": checked ON");
+        } else {
+            alert.setContentText(cbText + ": checked OFF");
+        }
+        alert.show();
+    }
+}
